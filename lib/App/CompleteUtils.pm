47dbd46a91dc51a056fcf2a34f281684408dc551
perlancar (@pc-office) <perlancar@gmail.com>
2014-12-26 23:50:23 +0700

    Initial commit

---
+++ lib/App/CompleteUtils.pm
@@ -0,0 +1,26 @@
+package App::CompleteUtils;
+
+# DATE
+# VERSION
+
+use 5.010001;
+use strict;
+use warnings;
+
+1;
+# ABSTRACT: Collection of CLI utilities for Dist::Zilla
+
+=head1 SYNOPSIS
+
+This distribution provides several command-line utilities related to
+(shell) completion and the L<Complete> modules family.
+
+
+=head1 SEE ALSO
+
+L<Complete>
+
+Frameworks you can test this with: L<Perinci::CmdLine>,
+L<Getopt::Long::Complete>, L<Getopt::Long::Subcommand>.
+
+=cut
